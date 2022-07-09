c3d011162c0949fd515c7d82c14e816ee9db9ac7
Satish Bharose <satishdbharose.hotmail.com>
2022-07-09 15:55:35 +0530

    first commit

---
+++ src/test/java/com/app/SpringJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.app;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case executing...");
+		assertThat(true);
+	}
+
+}
